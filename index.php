cb5ceddff55a23b0d721ce604cdf4ffa9d9c5ce1
Leonardo Davi <140842359+ldnrodrigues@users.noreply.github.com>
2024-03-08 11:13:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,67 +1,126 @@
 <?php get_header(); ?>
 
-    <div class="col mb-5">
-        <div id="custom-carousel" class="carousel slide">
-            <div class="carousel-inner">
-                <?php
-                $my_args_banner = array(
-                    'post_type' => 'banners',
-                    'posts_per_page' => 5,
-                );
-                $my_query_banner = new WP_Query($my_args_banner);
+<div class="col mb-5">
+    <!-- Carrossel de eventos para Banners -->
+    <div id="custom-carousel" class="carousel slide d-none d-sm-block">
+        <div class="carousel-inner">
+            <?php
+            $my_args_banner = array(
+                'post_type' => 'banners',
+                'posts_per_page' => 5,
+            );
+            $my_query_banner = new WP_Query($my_args_banner);
 
-                if ($my_query_banner->have_posts()) :
-                    $c = 0;
-                    while ($my_query_banner->have_posts()) :
-                        $my_query_banner->the_post();
-                ?>
-
-                        <div class="carousel-item <?php $c++;
-                            if ($c == 1) {
-                                echo ' active';
-                            } ?>">
+            if ($my_query_banner->have_posts()) :
+                $c = 0;
+                while ($my_query_banner->have_posts()) :
+                    $my_query_banner->the_post();
+            ?>
+                    <div class="carousel-item <?php echo $c === 0 ? 'active' : ''; ?>">
                         <?php
                         $banner_link = get_post_meta(get_the_ID(), 'link_banner', true);
-                            if (!empty($banner_link)) {
-                                echo '<a href="' . esc_url($banner_link) . '">';
-                            }
+                        if (!empty($banner_link)) {
+                            echo '<a href="' . esc_url($banner_link) . '">';
+                        }
                         the_post_thumbnail('post-thumbnail', array('class' => 'img-responsive border-0', 'style' => 'max-width: 100%;')) ?>
                     </div>
-
-                <?php endwhile;
-                    wp_reset_postdata();
-                endif; ?>
-            </div>
-
-            <ol class="carousel-indicators custom-indicators">
-                <?php
-                $slide_number = 0;
-                $my_query_banner = new WP_Query($my_args_banner);
-
-                while ($my_query_banner->have_posts()) {
-                    $my_query_banner->the_post();
-                    $slide_number++;
-
-                    // Define a classe "active" para o primeiro indicador
-                    $indicator_class = ($slide_number === 1) ? 'active' : '';
-
-                    echo '<li data-bs-target="#custom-carousel" data-bs-slide-to="' . esc_attr($slide_number - 1) . '" class="' . esc_attr($indicator_class) . '"></li>';
-                }
-
+            <?php
+                    $c++;
+                endwhile;
                 wp_reset_postdata();
-                ?>
-            </ol>
-
-            <button class="carousel-control-prev custom-control-button" role="button" data-bs-target="#custom-carousel" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Anterior</span>
-            </button>
-            <button class="carousel-control-next custom-control-button" role="button" data-bs-target="#custom-carousel" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Próximo</span>
-            </button>
+            endif;
+            ?>
         </div>
+
+        <ol class="carousel-indicators custom-indicators">
+            <?php
+            $slide_number = 0;
+            $my_query_banner = new WP_Query($my_args_banner);
+
+            while ($my_query_banner->have_posts()) {
+                $my_query_banner->the_post();
+                $slide_number++;
+
+                // Define a classe "active" para o primeiro indicador
+                $indicator_class = ($slide_number === 1) ? 'active' : '';
+
+                echo '<li data-bs-target="#custom-carousel" data-bs-slide-to="' . esc_attr($slide_number - 1) . '" class="' . esc_attr($indicator_class) . '"></li>';
+            }
+
+            wp_reset_postdata();
+            ?>
+        </ol>
+
+        <button class="carousel-control-prev custom-control-button" role="button" data-bs-target="#custom-carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Anterior</span>
+        </button>
+        <button class="carousel-control-next custom-control-button" role="button" data-bs-target="#custom-carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Próximo</span>
+        </button>
     </div>
+
+    <!-- Carrossel de eventos para Mobile -->
+    <div id="custom-mobile-carousel" class="carousel slide d-sm-none">
+        <div class="carousel-inner">
+            <?php
+            $my_args_mobile_banner = array(
+                'post_type' => 'mobile_banners',
+                'posts_per_page' => 5,
+            );
+            $my_query_mobile_banner = new WP_Query($my_args_mobile_banner);
+
+            if ($my_query_mobile_banner->have_posts()) :
+                $c = 0;
+                while ($my_query_mobile_banner->have_posts()) :
+                    $my_query_mobile_banner->the_post();
+            ?>
+                    <div class="carousel-item <?php echo $c === 0 ? 'active' : ''; ?>">
+                        <?php
+                        $banner_link = get_post_meta(get_the_ID(), 'link_banner', true);
+                        if (!empty($banner_link)) {
+                            echo '<a href="' . esc_url($banner_link) . '">';
+                        }
+                        the_post_thumbnail('post-thumbnail', array('class' => 'img-responsive border-0', 'style' => 'max-width: 100%;')) ?>
+                    </div>
+            <?php
+                    $c++;
+                endwhile;
+                wp_reset_postdata();
+            endif;
+            ?>
+        </div>
+
+        <ol class="carousel-indicators custom-indicators">
+            <?php
+            $slide_number = 0;
+            $my_query_mobile_banner = new WP_Query($my_args_mobile_banner);
+
+            while ($my_query_mobile_banner->have_posts()) {
+                $my_query_mobile_banner->the_post();
+                $slide_number++;
+
+                // Define a classe "active" para o primeiro indicador
+                $indicator_class = ($slide_number === 1) ? 'active' : '';
+
+                echo '<li data-bs-target="#custom-mobile-carousel" data-bs-slide-to="' . esc_attr($slide_number - 1) . '" class="' . esc_attr($indicator_class) . '"></li>';
+            }
+
+            wp_reset_postdata();
+            ?>
+        </ol>
+
+        <button class="carousel-control-prev custom-control-button" role="button" data-bs-target="#custom-mobile-carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Anterior</span>
+        </button>
+        <button class="carousel-control-next custom-control-button" role="button" data-bs-target="#custom-mobile-carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Próximo</span>
+        </button>
+    </div>
+</div>
 
 <!-- Page Content -->
 <div class="container mb-6" id="conteudo">
@@ -103,7 +162,7 @@
                 endif;
                 ?>
 
-                <div class="col-md-12 mb-4">
+                <div class="col-md-12 mb-4 container-noticias">
                     <div class="row">
                         <div class="col-md-6">
                             <?php if (has_post_thumbnail()) : ?>
@@ -126,10 +185,10 @@
                         <?php the_excerpt(); ?>
                     </div>
                     <a href="<?php the_permalink(); ?>" class="btn btn-lg" type="button" id="botao-noticias-1">Leia Mais</a>
-                    <p class="custom-text-color">
-                        por <?php the_author(); ?><br>
-                        <span><?php echo get_the_date('d/m/Y'); ?></span>
-                    </p>
+                    <h1 class="data-position-2 mt-2 custom-padding-text">
+                        Publicado por: <?php the_author(); ?><br>
+                        <span>Data: <?php echo get_the_date('d/m/Y'); ?></span>
+                    </h1>
             </div>
         </div>
     </div>
@@ -145,8 +204,12 @@
     </div>
 </div>
 
-<br>
-<hr style="border-top: 2px dashed red; width: 68%; margin: 0 auto;">
+
+    <!-- hr visível apenas em dispositivos desktop -->
+    <hr class="hr-text d-none d-sm-block mt-3" data-content="FIM DAS NOTÍCIAS RECENTES">
+
+    <!-- hr visível apenas em dispositivos móveis -->
+    <hr class="hr-text d-block d-sm-none mt-4" data-content="FIM DAS NOTÍCIAS RECENTES">
 
 <div class="d-flex justify-content-center">
     <a href="https://www.hml.tjrs.jus.br/novo/memorial-tjrs/noticias/"><button class="btn btn-lg" type="button" id="botao-noticias-2">Veja Todas as Notícias</button></a>
